0fd392016a6fc813cd67d762213cf6e427043a66
Vahan <vahan.grigoryan@gmail.com>
2024-03-03 14:42:14 +0400

    -

--- www/js/index.js
+++ www/js/index.js
@@ -128,11 +128,11 @@ function onDeviceReady() {
     // cutout.has().then(result => alert(JSON.stringify(result)));
 
 
-    // Promise.all([
-    //     LoadStyles('./../css/' + cssFile),
-    //     LoadScript('./../js/' + jsFile, null, 'body')
-    // ]).then(responses => {
-alert(App);
+    Promise.all([
+        LoadStyles('./../css/' + cssFile),
+        LoadScript('./../js/' + jsFile, null, 'body')
+    ]).then(responses => {
+
         App.InitializeApplication(
             'yerevan-parking',
             1,
@@ -149,7 +149,7 @@ alert(App);
             }
         );
 
-   // });
+   });
 
 
 }
